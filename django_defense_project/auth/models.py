4665c4c6a9ff7d5fd0d7867cebce4d0e6376fdc0
EmilAvramov <e_avramov@zoho.eu>
2023-03-21 00:17:41 +0200

    reinit user models and added bcrypt

--- django_defense_project/auth/models.py
+++ django_defense_project/auth/models.py
@@ -1,7 +1,47 @@
 from django.db import models
+from django.urls import reverse
 
 
 class User(models.Model):
-    email = models.EmailField(unique=True, max_length=64)
-    first_name = models.CharField(max_length=64)
-    last_name = models.CharField(max_length=64)
+    email = models.EmailField("Email", max_length=64, unique=True, db_index=True)
+    username = models.CharField("Username", max_length=64, unique=True, db_index=True)
+    password = models.CharField("Password", max_length=64)
+    first_name = models.CharField("First Name", max_length=64)
+    last_name = models.CharField("Last Name", max_length=64)
+
+    class Meta:
+        verbose_name = "Registered User"
+        verbose_name_plural = "Registered Users"
+
+    def __str__(self):
+        return f"${self.username}, ${self.email}, ${self.first_name} ${self.last_name}"
+
+    def get_absolute_url(self):
+        return reverse(
+            "user_detail",
+            kwargs={
+                "id": self.id,
+                "email": self.email,
+                "username": self.username,
+            },
+        )
+
+class Guest(models.Model):
+    username = models.CharField("Username", max_length=64, unique=True, db_index=True)
+
+    class Meta:
+        verbose_name = "Guest User"
+        verbose_name_plural = "Guest Users"
+
+    def __str__(self):
+        return f"${self.username}"
+
+    def get_absolute_url(self):
+        return reverse(
+            "guest_detail", 
+            kwargs={
+                "id": self.id, 
+                "username": self.username
+            }
+        )
+    
