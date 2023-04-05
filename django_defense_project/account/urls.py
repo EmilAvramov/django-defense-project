d0bc9f800a37a3b2e2d70418087635b14064d0fa
EmilAvramov <e_avramov@zoho.eu>
2023-04-06 01:06:38 +0300

    implemented basic login template, form, view

--- django_defense_project/account/urls.py
+++ django_defense_project/account/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path
 from . import views
+from django.urls import path
+from .views import Login
 
 app_name = "acc_app"
 
 urlpatterns = [
     path("register", views.register, name="register"),
-    path("login", views.login, name="login"),
-    path("profile", views.profile, name="profile")
+    path("login", Login.as_view(), name="login"),
+    path("profile", views.profile, name="profile"),
 ]
--- django_defense_project/account/views.py
+++ django_defense_project/account/views.py
@@ -1,10 +1,38 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.shortcuts import redirect
+from . import forms
+from django.contrib.auth import authenticate, login
+from django.views.decorators.csrf import csrf_protect
+from django.utils.decorators import method_decorator
+from django.http import HttpResponseForbidden
+
+
+class Login(TemplateView):
+    def __init__(self, **kwargs) -> None:
+        super().__init__(**kwargs)
+        self.form = forms.LoginForm()
+        self.template = "account/public/login.html"
+
+    def get(self, request):
+        return render(request, self.template, {"form": self.form})
+
+    @method_decorator(csrf_protect)
+    def post(self, request):
+        email = request.POST["email"]
+        password = request.POST["password"]
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect("search/digimon")
+        else:
+            error = "not authenticated"
+            return render(request, self.template, {"form": self.form, "error": error})
+
 
 def register(request):
-    return render(request, 'account/public/register.html', {})
+    return render(request, "account/public/register.html", {})
 
-def login(request):
-    return render(request, 'account/public/login.html', {})
 
 def profile(request):
-    return render(request, 'account/protected/profile.html', {})
+    return render(request, "account/protected/profile.html", {})
