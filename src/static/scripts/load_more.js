d72ad80190e1d0e3cbc2a66f0e795a4fd45631c8
EmilAvramov <e_avramov@zoho.eu>
2023-07-27 14:40:55 +0300

    tempale adjustment, bug fix to load more

--- src/static/scripts/load_more.js
+++ src/static/scripts/load_more.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 	var loadMoreBtn = document.getElementById('load_more__btn');
 	var searchResults = document.querySelector('.search__results');
+	var searchForm = document.querySelector('.search__form');
 
 	if (loadMoreBtn) {
 		loadMoreBtn.addEventListener('click', function () {
@@ -26,23 +27,47 @@ document.addEventListener('DOMContentLoaded', function () {
 					var items = data.content;
 					var fragment = document.createDocumentFragment();
 
-                    items.forEach(function (item) {
-                        var resultItem = document.createElement('div');
-                        resultItem.innerHTML = `<div class="search__result_card" data-pos="${item.id}">
-                                        <a href="/search/${item.id}/">
-                                          <div class="card__name">${item.name}</div>
-                                          <img class="card__image" src="${item.image}" alt="digimon image"/>
-                                        </a>
-                                      </div>`;
-                        fragment.appendChild(resultItem);
-                    });
+					items.forEach(function (item) {
+						var resultItem = document.createElement('div');
+						resultItem.innerHTML = `<div class="search__result_card" data-pos="${item.id}">
+									  <a href="/search/${item.id}/">
+										<div class="card__name">${item.name}</div>
+										<img class="card__image" src="${item.image}" alt="digimon image"/>
+									  </a>
+									</div>`;
+						fragment.appendChild(resultItem);
+					});
 
 					searchResults.innerHTML = '';
 					searchResults.appendChild(fragment);
 
+					if (!searchResults.querySelector('.search__heading')) {
+						var heading = document.createElement('h2');
+						heading.className = 'search__heading';
+						heading.textContent =
+							'Please enter a digimon name or ID below';
+						searchResults.insertBefore(
+							heading,
+							searchResults.firstChild
+						);
+					}
+
+					if (!searchResults.querySelector('.search__form')) {
+						var form = document.createElement('form');
+						form.className = 'search__form';
+						form.action = searchForm.dataset.url;
+						form.method = 'GET';
+						form.innerHTML = formHtml
+						searchResults.insertBefore(
+							form,
+							searchResults.firstChild.nextSibling
+						);
+					}
+
 					if (data.pageable.nextPage) {
 						var loadMoreBtn = document.createElement('button');
 						loadMoreBtn.id = 'load_more__btn';
+						loadMoreBtn.className = "blue__button"
 						loadMoreBtn.dataset.url = data.pageable.nextPage;
 						loadMoreBtn.textContent = 'Load More';
 						loadMoreBtn.addEventListener('click', function () {
